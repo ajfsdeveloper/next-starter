b2ff25a1acb34ed46d82e2923c3a2d8bd86b0867
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-12-24 16:57:58 +0530

    COMMIT-9: Styling components

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,45 @@ import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 import axios from 'axios'
 
+const ShowLink = ({ show }) => (
+  <li>
+    <Link as={`/p/${show.id}`} href={`/post?title=${show.id}`}>
+      <a>{show.name}</a>
+    </Link>
+  </li>
+)
+
 const Index = (props) => (
   <Layout>
     <h1>Batman TV Shows</h1>
     <ul>
       {props.shows.map(({ show }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
+        <ShowLink key={show.id} show={show} />
       ))}
     </ul>
+    <style global jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 )
 
