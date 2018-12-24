54f86930c7d36e32d3d4ef2b91ffa2f2528d1263
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-12-24 11:53:45 +0530

    COMMIT-4: Using Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Header from '../comps/Header'
+import Layout from '../components/MyLayout.js'
 
 export default () => (
-  <div>
-    <Header />
+  <Layout>
     <p>Hello Next.js</p>
-  </div>
+  </Layout>
 )
